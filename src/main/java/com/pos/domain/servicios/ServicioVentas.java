f83d5bf4f68034b6d52987c2be15ded855b7caae
DouCiv <cristian131412@gmail.com>
2026-07-14 19:47:04 -0500

    Completacion de clases pendientes

--- src/main/java/com/pos/domain/servicios/ServicioVentas.java
+++ src/main/java/com/pos/domain/servicios/ServicioVentas.java
@@ -1,5 +1,28 @@
 package main.java.com.pos.domain.servicios;
 
+import java.util.List;
+
+import main.java.com.pos.domain.ventas.entidades.Pago;
+import main.java.com.pos.domain.ventas.entidades.Venta;
+
 public class ServicioVentas {
  
+public Venta iniciarVenta(){
+
+}
+
+public void pagarVenta(Venta v, List <Pago> pagos){
+
+}
+
+public void suspenderVenta(Venta v){
+
+
+}
+
+public void anularVenta(Venta v, String motivo){
+
+}
+
+
 }
--- src/main/java/com/pos/presentation/controladores/ControladorVentas.java
+++ src/main/java/com/pos/presentation/controladores/ControladorVentas.java
@@ -1,5 +1,27 @@
 package main.java.com.pos.presentation.controladores;
 
+import main.java.com.pos.domain.servicios.ServicioVentas;
+import main.java.com.pos.domain.ventas.entidades.Venta;
+
 public class ControladorVentas {
  
+private Venta ventaActual;
+private ServicioVentas servicio;
+
+
+public void crearVenta(){
+
+}
+public void cerrarVenta(){
+
+}
+public void anulaVenta(String motivo){
+
+}
+public void agregarProducto(Producto p, int cantidad){//En espera de solucion de bug
+
+}
+
+
+
 }

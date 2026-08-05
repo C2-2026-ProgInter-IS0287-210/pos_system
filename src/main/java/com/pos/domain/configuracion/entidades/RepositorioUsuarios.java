cc6cfccf70d929f898c71f1ee333d22d6b27f67b
juan4298jose <juanjosediazlop8@gmail.com>
2026-08-04 19:34:55 -0500

    creacion de la interfas y correccion de la infrastructura

---
+++ src/main/java/com/pos/domain/configuracion/entidades/RepositorioUsuarios.java
@@ -0,0 +1,9 @@
+package com.pos.domain.configuracion.repositorios;
+
+import com.pos.domain.configuracion.entidades.Usuario;
+import util.List;
+
+public interface RepositorioUsuarios {
+    void guardar(Usuario u);
+   
+}
